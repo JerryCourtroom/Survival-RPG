14a42f101df11dbe5e8699bc643f2695b10c63f6
JerryCourtroom <jerrycourtroom123@gmail.com>
2023-01-08 15:59:09 -0500

    Customized subtitle and heading variable

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-var heading = document.getElementById("heading");
+window.addEventListener("load", main);
+
+var heading;
+
+function main() {
+    heading = document.getElementById("heading");
+}
+
 var screenWidth = window.innerWidth;
 var screenHeight = window.innerHeight;
 heading.style.left = 0.5 * screenWidth - 0.5 * heading.style.width;
